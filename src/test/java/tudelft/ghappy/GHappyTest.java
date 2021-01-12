e6fbfa66719ebd2ff2fe81ba755204870069e540
IStolen <ingvild.stoelen@gmail.com>
2021-01-12 11:20:42 +0100

    practical exercize #1

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,62 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void trueOneOccurance() {
+         GHappy muHap = new GHappy();
+         boolean result = muHap.gHappy("toggle");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void trueMultipleOccurences() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("toggleboggle");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void falseOneOcurrance() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("togood");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void falseMoreOcurrance() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("togoodgogo");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void falseBothOcurrance() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("toggoodgogo");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void falseThreeOcurrance() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("togggoodgogo");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void talseThreeOcurrance() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("togggoodggggoggggo");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void trueStartWithDouble() {
+        GHappy muHap = new GHappy();
+        boolean result = muHap.gHappy("ggopp");
+        Assertions.assertEquals(true, result);
+    }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,14 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.roman.RomanNumeral;
+
 public class MirrorTest {
+    @Test
+    public void number() {
+        Mirror mends = new Mirror();
+        String result = mends.mirrorEnds("adda");
+        Assertions.assertEquals("adda", result);
+    }
 }
